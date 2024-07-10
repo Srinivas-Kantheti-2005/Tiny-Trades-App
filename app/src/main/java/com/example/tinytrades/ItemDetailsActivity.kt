d889fefb6d01809a7267b086d1267d77c79fddad
narasimharaomarripalepu <143893393+narasimharaomarripalepu@users.noreply.github.com>
2024-07-10 10:57:21 +0530

    Update ItemDetailsActivity.kt
    Narasimha Rao

--- app/src/main/java/com/example/tinytrades/ItemDetailsActivity.kt
+++ app/src/main/java/com/example/tinytrades/ItemDetailsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tinytrades
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,15 +14,27 @@ class ItemDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_details)
 
+        val backbtn = findViewById<ImageButton>(R.id.backbtn)
         val itemImage: ImageView = findViewById(R.id.itemImage)
         val itemTitle: TextView = findViewById(R.id.itemTitle)
+        val itemSize: TextView = findViewById(R.id.itemSize)
         val itemPrice: TextView = findViewById(R.id.itemPrice)
+
+        backbtn.setOnClickListener {
+            onBackPressed()
+        }
 
         val bundle = intent.extras
         if (bundle != null) {
             itemImage.setImageResource(bundle.getInt("itemImage"))
             itemTitle.text = bundle.getString("itemTitle")
+            itemSize.text = bundle.getString("itemSize")
             itemPrice.text = bundle.getString("itemPrice")
         }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onBackPressed() {
+        finish()
     }
 }

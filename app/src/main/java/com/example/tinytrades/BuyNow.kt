7ab28f81fed4cbcdab7b841f91f02ce64bc1be75
kanth <srinivaskantheti394@gmail.com>
2024-07-31 21:41:44 +0530

    Buy now layout

---
+++ app/src/main/java/com/example/tinytrades/BuyNow.kt
@@ -0,0 +1,12 @@
+package com.example.tinytrades
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BuyNow : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_now)
+    }
+}
